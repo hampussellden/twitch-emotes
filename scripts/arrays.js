f6b4b84a52ab78e98808f1af3428232774a18b85
hampussellden <hampussellden1@gmail.com>
2022-12-12 15:00:22 +0100

    Made target/hunter responsive
    +Started making all emotes responsive

---
+++ scripts/arrays.js
@@ -0,0 +1,15 @@
+const emotes = [
+  ['images/bullets/4HEad-small.png', 'images/bullets/4HEad.png'],
+  ['images/bullets/4HEad-small.png', 'images/bullets/4HEad.png'],
+  ['images/bullets/4HEad-small.png', 'images/bullets/4HEad.png'],
+];
+let gifs = [
+  ['images/gifs/borpaspin.webp'],
+  ['images/gifs/modCheck.webp'],
+  ['images/gifs/pepegacard.webp'],
+  ['images/gifs/popCatRotate.webp'],
+  ['images/gifs/ratjam.webp'],
+  ['images/gifs/sadgeclap.webp'],
+  ['images/gifs/vicksyspin.webp'],
+  ['images/gifs/wickedsteer.webp'],
+];
